926b4e374a0476b7ecbb933713cf921cc3450f3a
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-05-18 21:44:31 +0200

    Add import aliases.

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@components", replacement: "/src/components/" },
+      { find: "@pages", replacement: "/src/pages/" },
+      { find: "@images", replacement: "/src/assets/images/" },
+      { find: "@style", replacement: "/src/styles" },
+      // { find: "", replacement: "" },
+    ],
+  },
+});
