4ac23da7b8ade51638e0d57bd7c960909cdf5636
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-05-21 09:15:40 +0200

    Add additional import aliases.

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: "@components", replacement: "/src/components/" },
-      { find: "@pages", replacement: "/src/pages/" },
-      { find: "@images", replacement: "/src/assets/images/" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@images", replacement: "/src/assets/images" },
       { find: "@style", replacement: "/src/styles" },
+      { find: "@info", replacement: "/src/info" },
+      { find: "@functions", replacement: "/src/functions" },
       // { find: "", replacement: "" },
     ],
   },
