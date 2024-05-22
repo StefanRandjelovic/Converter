83673fa7d288392b3b68a905e031330ffd3a2fbf
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-05-22 23:53:58 +0200

    Finish creating handleWeightCalculation function.

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -6,36 +6,159 @@ export const handleWeightCalculation = (
   converted
 ) => {
   if (isNaN(event.target.value)) {
-    setMessage("Enter a number.");
+    setMessage("Enter a number");
     return;
+  } else if (!event.target.value) {
+    setMessage("Enter a number");
   } else if (event.target.value < 0) {
     setMessage("Enter a number bigger or equal to zero.");
     return;
   } else if (event.target.value.length > 15) {
     return;
+  } else if (event.target.value) {
+    setMessage("");
   }
 
+  // Grams to...
   if (convert === "g" && converted === "g") {
     setWeight(event.target.value);
   } else if (convert === "g" && converted === "kg") {
     setWeight(event.target.value / 1000);
-  } else if (convert === "g" && converted === "ton") {
+  } else if (convert === "g" && converted === "metric ton") {
     setWeight(event.target.value / 1000000);
-  } else if (convert === "g" && converted === "lb") {
-    setWeight(event.target.value / 2204.62262);
+  } else if (convert === "g" && converted === "lbs") {
+    setWeight(event.target.value / 453.59);
   } else if (convert === "g" && converted === "oz") {
-    setWeight(event.target.value / 35273.9619);
+    setWeight(event.target.value / 28.35);
+  } else if (convert === "g" && converted === "short ton/American ton") {
+    setWeight(event.target.value / 907184.7408);
+  } else if (convert === "g" && converted === "long ton/Imperial ton") {
+    setWeight(event.target.value / 1016046.9084);
   }
 
+  // Kilograms to...
   if (convert === "kg" && converted === "g") {
-    setWeight((event.target.value / 1000).toFixed(2));
+    setWeight(event.target.value * 1000);
   } else if (convert === "kg" && converted === "kg") {
     setWeight(event.target.value);
-  } else if (convert === "kg" && converted === "ton") {
-    setWeight((event.target.value * 1000).toFixed(2));
-  } else if (convert === "kg" && converted === "lb") {
-    setWeight((event.target.value * 2.20462262).toFixed(2));
+  } else if (convert === "kg" && converted === "metric ton") {
+    setWeight(event.target.value / 1000);
+  } else if (convert === "kg" && converted === "lbs") {
+    setWeight(event.target.value * 2.20462262);
   } else if (convert === "kg" && converted === "oz") {
-    setWeight((event.target.value * 35.2739619).toFixed(2));
+    setWeight(event.target.value * 35.2739619);
+  } else if (convert === "kg" && converted === "short ton/American ton") {
+    setWeight(event.target.value / 907.18474);
+  } else if (convert === "kg" && converted === "long ton/Imperial ton") {
+    setWeight(event.target.value / 1016.0469);
+  }
+
+  // Metric ton to...
+  if (convert === "metric ton" && converted === "g") {
+    setWeight(event.target.value * 1000000);
+  } else if (convert === "metric ton" && converted === "kg") {
+    setWeight(event.target.value * 1000);
+  } else if (convert === "metric ton" && converted === "metric ton") {
+    setWeight(event.target.value);
+  } else if (convert === "metric ton" && converted === "lbs") {
+    setWeight(event.target.value * 2204.62262);
+  } else if (convert === "metric ton" && converted === "oz") {
+    setWeight(event.target.value * 35273.9619);
+  } else if (
+    convert === "metric ton" &&
+    converted === "short ton/American ton"
+  ) {
+    setWeight(event.target.value * 1.1023);
+  } else if (
+    convert === "metric ton" &&
+    converted === "long ton/Imperial ton"
+  ) {
+    setWeight(event.target.value / 1.0165);
+  }
+
+  // Pounds to...
+  if (convert === "lbs" && converted === "g") {
+    setWeight(event.target.value * 453.59237);
+  } else if (convert === "lbs" && converted === "kg") {
+    setWeight(event.target.value * 0.45359237);
+  } else if (convert === "lbs" && converted === "metric ton") {
+    setWeight(event.target.value * 0.0004536);
+  } else if (convert === "lbs" && converted === "lbs") {
+    setWeight(event.target.value);
+  } else if (convert === "lbs" && converted === "oz") {
+    setWeight(event.target.value * 16);
+  } else if (convert === "lbs" && converted === "short ton/American ton") {
+    setWeight(event.target.value * 0.0005);
+  } else if (convert === "lbs" && converted === "long ton/Imperial ton") {
+    setWeight(event.target.value * 0.0004464);
+  }
+
+  // Ounces to...
+  if (convert === "oz" && converted === "g") {
+    setWeight(event.target.value * 28.3495231);
+  } else if (convert === "oz" && converted === "kg") {
+    setWeight(event.target.value * 0.0283495231);
+  } else if (convert === "oz" && converted === "metric ton") {
+    setWeight(event.target.value * 2.835e-5);
+  } else if (convert === "oz" && converted === "lbs") {
+    setWeight(event.target.value / 16);
+  } else if (convert === "oz" && converted === "oz") {
+    setWeight(event.target.value);
+  } else if (convert === "oz" && converted === "short ton/American ton") {
+    setWeight(event.target.value / 32000);
+  } else if (convert === "oz" && converted === "long ton/Imperial ton") {
+    setWeight(event.target.value / 35839.72475);
+  }
+
+  // Short ton/American ton to...
+  if (convert === "short ton/American ton" && converted === "g") {
+    setWeight(event.target.value * 907184.74);
+  } else if (convert === "short ton/American ton" && converted === "kg") {
+    setWeight(event.target.value * 907.18474);
+  } else if (
+    convert === "short ton/American ton" &&
+    converted === "metric ton"
+  ) {
+    setWeight(event.target.value * 0.90718474);
+  } else if (convert === "short ton/American ton" && converted === "lbs") {
+    setWeight(event.target.value * 2000);
+  } else if (convert === "short ton/American ton" && converted === "oz") {
+    setWeight(event.target.value * 32000);
+  } else if (
+    convert === "short ton/American ton" &&
+    converted === "short ton/American ton"
+  ) {
+    setWeight(event.target.value);
+  } else if (
+    convert === "short ton/American ton" &&
+    converted === "long ton/Imperial ton"
+  ) {
+    setWeight(event.target.value * 0.892857143);
+  }
+
+  // Long ton/Imperial ton to...
+  if (convert === "long ton/Imperial ton" && converted === "g") {
+    setWeight(event.target.value * 1016046.91);
+  } else if (convert === "long ton/Imperial ton" && converted === "kg") {
+    setWeight(event.target.value * 1016.0469);
+  } else if (
+    convert === "long ton/Imperial ton" &&
+    converted === "metric ton"
+  ) {
+    setWeight(event.target.value * 1.016046);
+  } else if (convert === "long ton/Imperial ton" && converted === "lbs") {
+    setWeight(event.target.value * 2240);
+  } else if (convert === "long ton/Imperial ton" && converted === "oz") {
+    setWeight(event.target.value * 35840);
+  } else if (
+    convert === "long ton/Imperial ton" &&
+    converted === "short ton/American ton"
+  ) {
+    setWeight(event.target.value * 1.12);
+  } else if (
+    convert === "long ton/Imperial ton" &&
+    converted === "long ton/Imperial ton"
+  ) {
+    setWeight(event.target.value);
   }
 };
