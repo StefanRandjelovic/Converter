2963e00af234c4826e63c7e4e1438bc87b2b8f17
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-05-27 21:54:24 +0200

    Add labels for the imports and an additional route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
+// DEV DEPENDENCY
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// PAGES
 import WeightConvert from "./pages/WeightConvert";
-
+import Landing from "@pages/Landing";
 
 function App() {
-  
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <WeightConvert  />
-          }
-        />
-        <Route />
+        <Route path="/" element={<Landing />} />
+        <Route path="/weightConvert" element={<WeightConvert />} />
       </Routes>
     </Router>
   );
