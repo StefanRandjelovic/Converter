e640f18733c9a36234f5f0f5bd42169ae91afc23
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-05-27 21:58:04 +0200

    Create Landing page to represent the initial screen of an app.

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,27 @@
+// DEV DEPENDENCY
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// COMPONENTS
+import Title from "@components/Title";
+// STYLES
+import "@style/landing.scss";
+
+const Landing = () => {
+  const [loading, setLoading] = useState(true);
+  setTimeout(() => setLoading(false), 1500);
+
+  return (
+    <main>
+      {loading && <h1 id="welcomeTitle">Welcome...</h1>}
+      {!loading && (
+        <>
+          <Title title="Converter app" cls="landing" />
+          <p>Please choose from one of these converters.</p>
+          <Link to={"/weightConvert"}>Weight converter</Link>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default Landing;
