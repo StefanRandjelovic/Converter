aee8084d0394a7490622674dce24a4e9dc28a926
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-05-30 21:10:22 +0200

    Implement style file aswell as another link.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,16 @@
 import { Link, useNavigate } from "react-router-dom";
 // IMAGE
 import Chevron from "@images/chevron.svg";
+// STYLE
+import "@style/header.scss";
 
 const Header = () => {
+  const location = useNavigate();
   return (
     <header>
-      <img src={Chevron} alt="back" />
+      <img src={Chevron} alt="back" onClick={() => location(-1)} />
       <Link to={"/weightConvert"}>Weight Converter</Link>
+      <Link to={"/waterNeeds"}>How much pesticide do I need?</Link>
     </header>
   );
 };
